99e71df8367ed5423a6632b18b274e9369deddd0
Mohaiminul-Islam-Bhuiyan <rafe.aust@gmail.com>
2022-09-04 00:45:03 +0600

    catagory section completed

--- js/index.js
+++ js/index.js
@@ -1,19 +1,29 @@
-const loadNews = async() => {
+const loadCatagory = async() => {
     const url = 'https://openapi.programming-hero.com/api/news/categories'
     const res = await fetch(url);
     const data = await res.json();
     displayNewsCatagory(data.data.news_category);
 }
 
+
 const displayNewsCatagory = data => {
     const catagories = document.getElementById('catagory')
     for (const catagory of data){
         const li = document.createElement('li')
         li.innerHTML = `
-        <a>${catagory.category_name}</a>
+        <button onclick="loadNews('${catagory.category_id}')" class='btn btn-primary'>${catagory.category_name}</button>
         `
         catagories.appendChild(li)
     }
 }
 
-loadNews()
+const loadNews = async(category_id) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(data.data)
+}
+
+
+
+loadCatagory()
