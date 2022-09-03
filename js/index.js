dc9e2abbe53dc31b37695aedceb692b887563623
Mohaiminul-Islam-Bhuiyan <rafe.aust@gmail.com>
2022-09-04 04:56:53 +0600

    news number added

--- js/index.js
+++ js/index.js
@@ -28,7 +28,14 @@ const loadNews = async(category_id) => {
     const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`
     const res = await fetch(url)
     const data = await res.json()
-    // console.log(data.data)
+    const dataArray = data.data
+    const newsNumberField = document.getElementById('news-number')
+    if(dataArray.length === 0){
+        newsNumberField.value = 'No news found';
+    }
+    else{
+        newsNumberField.value = 'Total news found: '+ dataArray.length;
+    }
     displayNews(data.data)
 }
 
