0896ec08c785f4dce4d8369a99e4d93a8c735953
Mohaiminul-Islam-Bhuiyan <rafe.aust@gmail.com>
2022-09-03 22:19:32 +0600

    catagory added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-console.log('connected')
+const loadNews = async() => {
+    const url = 'https://openapi.programming-hero.com/api/news/categories'
+    const res = await fetch(url);
+    const data = await res.json();
+    displayNewsCatagory(data.data.news_category);
+}
+
+const displayNewsCatagory = data => {
+    const catagories = document.getElementById('catagory')
+    for (const catagory of data){
+        const li = document.createElement('li')
+        li.innerHTML = `
+        <a>${catagory.category_name}</a>
+        `
+        catagories.appendChild(li)
+    }
+}
+
+loadNews()
