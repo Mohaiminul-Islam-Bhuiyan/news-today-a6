75a79a0bdd57f38f101dbff2e7cfb9a12d1814a1
Mohaiminul-Islam-Bhuiyan <rafe.aust@gmail.com>
2022-09-04 04:07:58 +0600

    try catch added

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
-const loadCatagory = async() => {
-    const url = 'https://openapi.programming-hero.com/api/news/categories'
-    const res = await fetch(url);
-    const data = await res.json();
-    displayNewsCatagory(data.data.news_category);
-}
+
+async function loadCatagory(){
+    try {
+        const url = 'https://openapi.programming-hero.com/api/news/categories'
+        const res = await fetch(url);
+        const data = await res.json();
+        displayNewsCatagory(data.data.news_category);
+    }
+    catch(error){
+        alert(error)
+    }
+    }
 
 
 const displayNewsCatagory = data => {
